c0aa939a8ca83a75337959731089f4b8f4bb1152
Richard Todd <richard@waywardcode.com>
2016-04-18 00:20:43 -0500

    made any row of cards possible.

--- main.go
+++ main.go
@@ -1,9 +1,6 @@
 package main
 
 import (
-	"archive/zip"
-	"fmt"
-	"github.com/nfnt/resize"
 	"image"
 	"image/draw"
 	"image/jpeg"
@@ -14,29 +11,27 @@ import (
 	"time"
 )
 
+// hold a global cached deck between requests...
+var curDeck *deck
+
+func requestDeck(name string) (*deck, error) {
+	if curDeck != nil && curDeck.Name() == name {
+		return curDeck, nil
+	}
+
+	if curDeck != nil {
+		curDeck.Close()
+	}
+	var err error
+	curDeck, err = NewDeck(name)
+	return curDeck, err
+}
+
 func main() {
 	rand.Seed(time.Now().UnixNano())
 
-	http.HandleFunc("/three-card/", threeHandler)
+	http.HandleFunc("/row/", rowHandler)
 	log.Fatal(http.ListenAndServe("localhost:8000", nil))
-}
-
-func getRatio(imgs []*zip.File) (float64, error) {
-	if len(imgs) < 1 {
-		return 0.0, fmt.Errorf("No images in zip file!")
-	}
-	img, err := imgs[0].Open()
-	if err != nil {
-		return 0.0, err
-	}
-	defer img.Close()
-
-	dec, err := jpeg.Decode(img)
-	if err != nil {
-		return 0.0, err
-	}
-	b := dec.Bounds()
-	return (float64(b.Dx()) / float64(b.Dy())), nil
 }
 
 func getOrElse(lst []string, def string) string {
@@ -46,55 +41,57 @@ func getOrElse(lst []string, def string) string {
 	return def
 }
 
-func threeHandler(w http.ResponseWriter, r *http.Request) {
+func rowHandler(w http.ResponseWriter, r *http.Request) {
 	if err := r.ParseForm(); err != nil {
 		log.Print(err)
 	}
 
-	desiredWidth, _ := strconv.Atoi(getOrElse(r.Form["w"], "600"))
+	desiredWidth, _ := strconv.Atoi(getOrElse(r.Form["width"], "600"))
+	desiredCards, _ := strconv.Atoi(getOrElse(r.Form["cards"], "3"))
+	desiredShowing, _ := strconv.Atoi(getOrElse(r.Form["pct"], "100"))
 	desiredDeck := getOrElse(r.Form["deck"], "Lenormand")
-	deck, err := zip.OpenReader(desiredDeck + ".zip")
+	log.Printf("ROW:  Deck: %s Cards: %d  Width: %d  Showing: %d%%",
+		desiredDeck,
+		desiredCards,
+		desiredWidth,
+		desiredShowing)
+
+	deck, err := requestDeck(desiredDeck + ".zip")
 	if err != nil {
 		log.Print(err)
 		return
 	}
-	defer deck.Close()
 
-	// grab the first file and establish the aspect ratio of a card
-	// (we will assume all cards are roughly the same shape)
-	rat, err := getRatio(deck.File)
+	// to account for overlap, we figure out the number of
+	// cards effectively showing.  Thus 3 cards showing at 100%
+	// would be 1 + 1 + 1, while at 80% it would be .8 + .8 + 1
+	// (since the last card is fully visible)
+	showPct := float64(desiredShowing) / 100.0
+	effectiveCards := 1.0 + float64(desiredCards-1)*showPct
+	cardWidth := uint(float64(desiredWidth) / effectiveCards)
+	cardHeight := deck.CardHeight(cardWidth)
+	showingWidth := int(float64(cardWidth) * showPct)
+
+	// now, shuffle the deck
+	selected, err := deck.Shuffled(desiredCards)
 	if err != nil {
 		log.Print(err)
 		return
 	}
 
-	cardWidth := (desiredWidth / 3)
-	cardHeight := int(float64(cardWidth) / rat)
-	shuffled := rand.Perm(len(deck.File))
-	selected := shuffled[:3]
-
-	answer := image.NewRGBA(image.Rect(0, 0, cardWidth*len(selected), cardHeight))
+	// now, create the image
+	actualWidth := int(effectiveCards * float64(cardWidth))
+	answer := image.NewRGBA(image.Rect(0, 0, actualWidth, int(cardHeight)))
 	for idx, c := range selected {
-		xloc := idx * cardWidth
-		cardRect := image.Rect(xloc, 0, xloc+cardWidth, cardHeight)
+		xloc := idx * showingWidth
+		cardRect := image.Rect(xloc, 0, xloc+int(cardWidth), int(cardHeight))
 
 		// open the card...
-		cardFile, err := deck.File[c].Open()
+		cardImg, err := deck.Image(c, cardWidth, nil)
 		if err != nil {
 			log.Print(err)
 			return
 		}
-
-		// read the JPG inside
-		cardImg, err := jpeg.Decode(cardFile)
-		cardFile.Close()
-		if err != nil {
-			log.Print(err)
-			return
-		}
-
-		// resize it...
-		cardImg = resize.Resize(uint(cardWidth), uint(cardHeight), cardImg, resize.Bicubic)
 
 		draw.Draw(answer, cardRect, cardImg, image.Pt(0, 0), draw.Src)
 	}
